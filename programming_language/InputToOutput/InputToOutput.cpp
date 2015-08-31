87f1b4b9e0193c824a0556cd9c54d2d84b5ea012
xiaoxu <152928753@qq.com>
2015-08-31 22:45:56 +0800

    InputToOutput

---
+++ programming_language/InputToOutput/InputToOutput.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+*编写一个将输入复制到输出的程序，并且将其中的多个空格用一个空格代替
+*
+*/
+
+int main()
+{
+	/*
+	Input
+	*/
+	char Charactor;
+	int inspace = 0;
+	while ((Charactor = getchar()) != -1)
+	{
+		if (Charactor == ' ')
+		{
+			
+			if (inspace == 0)
+			{
+				putchar('x');
+				inspace = 1;
+
+			}
+		}
+		if (Charactor != ' ')
+		{
+			putchar(Charactor);
+			inspace = 0;
+		}
+
+	}
+
+
+	return 0;
+}
